fdeec2a7d49ccbe21e75d91d0c79bb8ac48b0df1
Alejandro Castillo <acastillocampillo@gmail.com>
2023-07-13 20:37:45 +0200

    merge

--- src/modules/admins/routes.js
+++ src/modules/admins/routes.js
@@ -1,21 +1,12 @@
 export default [
-<<<<<<< HEAD
   {
     path: "/admins",
     name: "Admins",
     component: async () => await import('@/modules/admins/pages/Admins.vue')
+  },
+  {
+    path: "/admins/nuevoUsuario",
+    name: "nuevoUsuario",
+    component: async () => await import('@/modules/admins/pages/newUser.vue')
   }
 ]
-=======
-    {
-        path: "/admins",
-        name:"Admins",
-        component:async()=> await import('@/modules/admins/pages/Admins.vue')
-    },
-    {
-        path: "/admins/nuevoUsuario",
-        name:"nuevoUsuario",
-        component:async()=> await import('@/modules/admins/pages/newUser.vue')
-    }
-]
->>>>>>> 1d749eb6319078c064f992e7fbab6136e494a27f
