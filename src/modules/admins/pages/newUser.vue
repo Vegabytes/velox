1d749eb6319078c064f992e7fbab6136e494a27f
jorgeld <jorgeld1986@gmail.com>
2023-07-13 20:27:12 +0200

    Feature (nuevo usuario): Crear vista nuevo usuario
    Creamos formulario para la creación de nuevos usuarios desde la parte admin

---
+++ src/modules/admins/pages/newUser.vue
@@ -0,0 +1,83 @@
+<template>
+    <v-container>
+        <v-card color="white" class="d-flex justify-start">
+            <v-col>
+                <v-card-title class="pa-6">
+                <h2>Nuevo Usuario</h2>
+            </v-card-title>
+            <v-card-text>
+                
+                    <form @submit.prevent="submit">
+
+
+                        <v-row>
+                            <v-col cols="12" md="6">
+                                <v-text-field :counter="20" label="Nombre" variant="outlined" color="primary"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" md="6">
+                                <v-text-field :counter="20" label="Apellidos" variant="outlined" color="primary"></v-text-field>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12" md="6">
+                                <v-text-field :counter="20" label="Alias" variant="outlined" color="primary"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" md="6">
+                                <v-text-field :counter="20" label="Fecha de nacimiento" variant="outlined" color="primary"></v-text-field>
+                            </v-col>
+                        </v-row>
+
+                        <v-row>
+                            <v-col cols="12">
+                                <v-text-field :counter="20" label="Dirección" variant="outlined" color="primary"></v-text-field>
+                            </v-col>
+                        </v-row>
+
+                        <v-row>
+                            <v-col cols="12" md="6">
+                                <v-text-field :counter="20" label="Correo electrónico" variant="outlined" color="primary"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" md="6">
+                                <v-text-field :counter="20" label="Teléfono" variant="outlined" color="primary"></v-text-field>
+                            </v-col>
+                        </v-row>
+
+                        <v-row>
+                            <v-col cols="12" md="6">
+                                <v-select :items="items" label="Tipo de Usuario" variant="outlined" color="primary"></v-select>
+                            </v-col>
+                            <v-col cols="12" md="6">
+                                <v-select :items="items" label="Grupo al que pertenece" variant="outlined" color="primary"></v-select>
+                            </v-col>
+                        </v-row>
+
+                        <v-row>
+                            <v-col cols="12">
+                                <v-textarea :counter="20" label="Descripción" variant="outlined" color="primary"></v-textarea>
+                            </v-col>
+                        </v-row>
+
+                        <v-row>
+                            <v-col cols="12">
+                                <v-btn class="me-4" type="submit" variant="tonal" rounded="xl" size="x-large">
+                                    Crear usuario
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </form>
+            </v-card-text>
+            </v-col>
+            
+        </v-card>
+    </v-container>
+</template>
+
+<script setup>
+
+import { useAppStore } from '@/store/index';
+const appStore = useAppStore()
+appStore.showMenu = true
+
+
+
+</script>
--- src/modules/admins/routes.js
+++ src/modules/admins/routes.js
@@ -3,5 +3,10 @@ export default [
         path: "/admins",
         name:"Admins",
         component:async()=> await import('@/modules/admins/pages/Admins.vue')
+    },
+    {
+        path: "/admins/nuevoUsuario",
+        name:"nuevoUsuario",
+        component:async()=> await import('@/modules/admins/pages/newUser.vue')
     }
 ]
