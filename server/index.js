a4417e7eb23e36f0b1201d1addf70c778ac67dbf
Alejandro Castillo <acastillocampillo@gmail.com>
2023-07-22 20:52:13 +0200

    Chore(Server) Fallback para variables de entorno

--- server/index.js
+++ server/index.js
@@ -12,6 +12,18 @@ app.use(cors())
 app.use(cors({
   origin: 'http://185.166.213.42'
 }));
+
+// CORS
+app.use(function (req, res, next) {
+  /*   res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); */
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
