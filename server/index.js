c62131cec76f58c8c818db8b5ea082daebd88aa0
Alejandro Castillo <acastillocampillo@gmail.com>
2023-07-26 11:31:22 +0200

    Fix(Server)  Cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ const PORT = process.env.PORT || 5000;
 
 /* app.use(cors()) */
 
+// CORS
 app.use(function (req, res, next) {
+  /*   res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); */
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
