1bc6af679780dcebd6463fbe357a64bbe47e11fb
Alejandro Castillo <acastillocampillo@gmail.com>
2023-07-26 11:06:43 +0200

    Fix(server) cors

--- server/index.js
+++ server/index.js
@@ -7,16 +7,16 @@ import 'dotenv/config'
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors())
+/* app.use(cors()) */
 
-/* app.use(function (req, res, next) {
+app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 
   next();
-}); */
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
