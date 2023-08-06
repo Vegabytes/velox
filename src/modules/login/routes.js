ee2083d33637a896fd88edba0efaebcb7e78d7fd
Alejandro Castillo <acastillocampillo@gmail.com>
2023-08-06 20:13:54 +0200

    Fix(Frontend) Error importar modulo

--- src/modules/login/routes.js
+++ src/modules/login/routes.js
@@ -8,10 +8,5 @@ export default [
     path: "/:idGroup/login",
     name: "Login",
     component: async () => await import('@/modules/login/pages/Login')
-  },
-  {
-    path: "/create",
-    name: "Create",
-    component: async () => await import('@/modules/login/pages/Create')
-  },
+  }
 ]
