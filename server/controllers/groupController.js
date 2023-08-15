494ce680a1f4cebb058946bf53bf7765f413a075
Alejandro Castillo <acastillocampillo@gmail.com>
2023-08-15 12:27:05 +0200

    Feature(Global) Implementar pantalla de dispositivos

--- server/controllers/groupController.js
+++ server/controllers/groupController.js
@@ -23,18 +23,72 @@ export const getGroupByGroupIdByUserId = async (req, res) => {
 
   try {
     const gruposByUserId = await getGroupsByUserId(userId);
-    console.log("gruposByUserId", gruposByUserId);
 
     let gruposUsuarios = await Promise.all(
       gruposByUserId.map(async (element) => {
         return await getGroupInfoByParent(element, groupId);
       })
     );
+
+
+
+
+    /*   console.log("gruposDevices", gruposDevices); */
+
+
     const gruposUsuariosNotNull = gruposUsuarios.filter(user => !!user);
+
+    gruposUsuariosNotNull.forEach(element => {
+      element['devicesGroups'] = [], element['devices'] = []
+    }
+    );
+    //Obtenemos grupos de dispositivo por cada grupo de usuario
+    let gruposDevices = await Promise.all(
+      gruposUsuariosNotNull.map(async (element) => {
+        console.log("element", element);
+        return await getDevicesGroupsByUserGroupId(element);
+      })
+    )
+    const flattenDeep = (arr1) => {
+      return arr1.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
+    }
+    gruposDevices = flattenDeep(gruposDevices);
+
+    gruposUsuariosNotNull.forEach(gu => {
+
+      console.log("gu", gu);
+      gruposDevices.map(gd => {
+        if (gd.userGroupId === gu.id) gu.devicesGroups.push(gd);
+      })
+    });
+
+
+
+    await Promise.all(
+      gruposUsuariosNotNull.map(async gu => {
+        return await Promise.all(gu.devicesGroups.map(async gd => {
+          const devices = await getDeviceIdBygroupId(gd);
+
+          let _devices = {}
+
+          devices.forEach(d => {
+            if (!_devices[d.id]) {
+              _devices[d.id] = d
+            }
+          })
+
+          gu.devices.push(Object.values(_devices)[0])
+
+          return devices;
+        })
+        )
+      })
+    )
     res.status(200).send(gruposUsuariosNotNull)
 
 
   } catch (error) {
+    console.log(error);
     res.status(500).send(error)
   }
 }
@@ -99,13 +153,11 @@ const getGroupInfo = ({ groupId }) => {
 
 const getGroupInfoByParent = ({ groupId }, parentGroupId) => {
   return new Promise((resolve, reject) => {
-    console.error("groupId", groupId);
-    console.error("parentGroupId", parentGroupId);
     connection.query('SELECT * FROM UserGroups WHERE id = ? and parentGroupId = ?', [groupId, parentGroupId], (error, elements) => {
       if (error) {
         return reject(error);
       }
-      console.log("Elements", elements);
+
       return resolve(elements[0]);
     });
   });
@@ -141,7 +193,28 @@ export const associateUserUserGroup = async (req, res) => {
 }
 
 
+const getDevicesGroupsByUserGroupId = ({ id }) => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM DeviceGroupUserGroup WHERE userGroupId = ?', [id], (error, elements) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(elements);
+    });
+  });
+};
 
+const getDeviceIdBygroupId = ({ deviceGroupId }) => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM DeviceGroupMembers INNER JOIN Devices WHERE deviceId = ? and Devices.id = DeviceGroupMembers.deviceId', [deviceGroupId], (error, elements) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(elements);
+    });
+  });
+
+};
 
 
 
