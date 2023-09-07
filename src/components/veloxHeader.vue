af5fb993577384907e9ea50b7574e1358a63b185
Alejandro Castillo <acastillocampillo@gmail.com>
2023-09-07 18:01:48 +0200

    Feature(header) Pegar a la parte superior

--- src/components/veloxHeader.vue
+++ src/components/veloxHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <v-row>
     <v-col md="1" cols="12" class="d-flex justify-center align-center">
-      <v-img class="mx-auto my-6" max-width="80" src="@/assets/logo.png" />
+      <v-img class="" max-width="80" src="@/assets/logo.png" />
     </v-col>
-    <v-col md="11" cols="12" class="d-flex justify-center align-center py-0">
+    <v-col md="11" cols="12" class="d-flex justify-center py-0">
 
-      <v-img :src="`${path}/${props.path}`" class="align-end rounded-bs-xl" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-        height="150px" cover>
+      <v-img :src="`${path}/${props.path}`" class="align-end rounded-bs-xl"
+        gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="150px" cover>
         <v-card-title class="text-white text-h2" v-text="props.name"></v-card-title>
         <v-card-subtitle class="text-white text-h5 mb-4" v-text="props.description"></v-card-subtitle>
       </v-img>
@@ -17,6 +17,6 @@
 
 <script setup>
 import { ref } from 'vue';
-const props = defineProps(['path','name','description'])
+const props = defineProps(['path', 'name', 'description'])
 const path = ref(import.meta.env['VITE_SERVER_BASE_URL'] || 'http://185.166.213.42:5000');
 </script>
